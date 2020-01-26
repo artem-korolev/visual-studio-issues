e0843a3785942d8e1ae0bc6fe24938c330ac894e
Artem Korolev <artkor@live.ru>
2020-01-26 16:51:43 +0200

    defining first service - SignUp

--- src/test/test-client/test-client.cpp
+++ src/test/test-client/test-client.cpp
@@ -27,35 +27,31 @@
 using grpc::Channel;
 using grpc::ClientContext;
 using grpc::Status;
-using helloworld::HelloRequest;
-using helloworld::HelloReply;
-using helloworld::Greeter;
+using GreenLight::SignUpForm;
+using GreenLight::Session;
+using GreenLight::LoginService;
 
-class GreeterClient {
+class TestClient {
 public:
-    GreeterClient(std::shared_ptr<Channel> channel)
-        : stub_(Greeter::NewStub(channel)) {}
+    TestClient(std::shared_ptr<Channel> channel)
+        : stub_(LoginService::NewStub(channel)) {}
 
     // Assembles the client's payload, sends it and presents the response back
     // from the server.
-    std::string SayHello(const std::string& user) {
-        // Data we are sending to the server.
-        HelloRequest request;
-        request.set_name(user);
-
+    std::string signUp(const SignUpForm& signupForm) {
         // Container for the data we expect from the server.
-        HelloReply reply;
+        Session reply;
 
         // Context for the client. It could be used to convey extra information to
         // the server and/or tweak certain RPC behaviors.
         ClientContext context;
 
         // The actual RPC.
-        Status status = stub_->SayHello(&context, request, &reply);
+        Status status = stub_->SignUp(&context, signupForm, &reply);
 
         // Act upon its status.
         if (status.ok()) {
-            return reply.message();
+            return reply.session();
         }
         else {
             std::cout << status.error_code() << ": " << status.error_message()
@@ -65,7 +61,7 @@ public:
     }
 
 private:
-    std::unique_ptr<Greeter::Stub> stub_;
+    std::unique_ptr<LoginService::Stub> stub_;
 };
 
 int main(int argc, char** argv) {
@@ -73,14 +69,21 @@ int main(int argc, char** argv) {
     // are created. This channel models a connection to an endpoint (in this case,
     // localhost at port 50051). We indicate that the channel isn't authenticated
     // (use of InsecureChannelCredentials()).
-    GreeterClient greeter(grpc::CreateChannel(
+    TestClient greeter(grpc::CreateChannel(
         "localhost:50051", grpc::InsecureChannelCredentials()));
-    std::string user(250000, '*');
+
+    // Prepare data to be send to server
+    SignUpForm signupForm;
+    signupForm.set_email("test@test.com");
+    signupForm.set_firstname("Test");
+    signupForm.set_firstname("Testovich");
+    signupForm.set_password("test");
+
     int messageCounter = 0;
     while (true) {
-        std::string reply = greeter.SayHello(user);
+        std::string reply = greeter.signUp(signupForm);
         if (messageCounter++ % 1000 == 0) {
-            std::cout << "Greeter received: (" << messageCounter << "): " << std::endl;
+            std::cout << "Greeter received: (" << messageCounter << "): " << reply << std::endl;
         }
     }
 
---
+++ src/test/test-server/test-server.cpp
@@ -0,0 +1,90 @@
+// test.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <memory>
+#include <string>
+
+#include <grpcpp/grpcpp.h>
+
+#include "greenlight.grpc.pb.h"
+
+using grpc::Server;
+using grpc::ServerBuilder;
+using grpc::ServerContext;
+using grpc::Status;
+using GreenLight::SignUpForm;
+using GreenLight::Session;
+using GreenLight::LoginService;
+
+std::string random_string(size_t length)
+{
+	auto randchar = []() -> char
+	{
+		const char charset[] =
+			"0123456789"
+			"ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+			"abcdefghijklmnopqrstuvwxyz";
+		const size_t max_index = (sizeof(charset) - 1);
+		return charset[rand() % max_index];
+	};
+	std::string str(length, 0);
+	std::generate_n(str.begin(), length, randchar);
+	return str;
+}
+
+// Logic and data behind the server's behavior.
+class GreeterServiceImpl final : public LoginService::Service {
+	int messageCounter = 0;
+
+
+	/*
+	 * SignUp
+	 */
+	Status SignUp(ServerContext* context, const SignUpForm* request, Session* reply) override {
+		std::string prefix("Hello ");
+		if (messageCounter++ % 1000 == 0) {
+			std::cout << "received message( " << messageCounter << ")" << std::endl;
+		}
+		reply->set_session(random_string(32));
+		return Status::OK;
+	}
+
+
+};
+
+void RunServer() {
+	std::string server_address("0.0.0.0:50051");
+	GreeterServiceImpl service;
+
+	ServerBuilder builder;
+	// Listen on the given address without any authentication mechanism.
+	builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
+	// Register "service" as the instance through which we'll communicate with
+	// clients. In this case it corresponds to an *synchronous* service.
+	builder.RegisterService(&service); 
+	// Finally assemble the server.
+	std::unique_ptr<Server> server(builder.BuildAndStart());
+	std::cout << "Server listening on " << server_address << std::endl;
+
+	// Wait for the server to shutdown. Note that some other thread must be
+	// responsible for shutting down the server for this call to ever return.
+	server->Wait();
+}
+
+int main(int argc, char** argv) {
+	RunServer();
+
+	return 0;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
